0089765c95d4452b21b2e1182f45a5a27ae90c5d
vnfma0218 <vnfma0218@naver.com>
2023-07-02 01:45:09 +0900

    feat: cors에러 처리, express-validator 설치 및 적용

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
 const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
 const app = express();
 const port = 8080;
 const db = require('./db.js'); // db 불러오기
+
+const whitelist = ['http://localhost:3000'];
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin || whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+app.use(bodyParser.json());
 
 // 라우터 모듈
 const indexRouter = require('./routes');
--- routes/post.js
+++ routes/post.js
@@ -1,7 +1,17 @@
+const { body, param, validationResult } = require('express-validator');
 const express = require('express');
 const Post = require('../models/post');
 
 const router = express.Router();
+
+// 검사 미들웨어 분리
+const validate = (req, res, next) => {
+  const errors = validationResult(req);
+  if (errors.isEmpty()) {
+    return next();
+  }
+  return res.status(400).json({ message: errors.array()[0].msg });
+};
 
 router.get('/', async (req, res, next) => {
   try {
@@ -13,20 +23,32 @@ router.get('/', async (req, res, next) => {
     next(err);
   }
 });
-router.post('/new', async (req, res, next) => {
-  try {
-    console.log(req.body);
-    const post = await Post.create({
-      title: req.body.name,
-      content: req.body.age,
-      commentCount: 0,
-    });
-    res.json('post list');
-    // res.render('mongoose', { users });
-  } catch (err) {
-    console.error(err);
-    next(err);
+router.post(
+  '/new',
+  [
+    body('title')
+      .trim()
+      .isLength({ min: 5, max: 30 })
+      .withMessage('제목은 5~30자를 입력해주세요.'),
+    body('content').notEmpty().withMessage('내용을 입력해주세요.'),
+    validate,
+  ],
+  async (req, res, next) => {
+    try {
+      console.log('req.body', req.body);
+      const newPost = await Post.create({
+        title: req.body.title,
+        content: req.body.content,
+        commentCount: 0,
+      });
+
+      res.status(201).json(newPost);
+      // res.render('mongoose', { users });
+    } catch (err) {
+      console.error(err);
+      next(err);
+    }
   }
-});
+);
 
 module.exports = router;
